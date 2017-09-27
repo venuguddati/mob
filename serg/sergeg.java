812986f46a952111ba8f0f4cf16f1196f488a817
venuguddati <venugopal3505@gmail.com>
2017-09-27 12:42:34 +0530

    serg commit

---
+++ serg/sergeg.java
@@ -0,0 +1,11 @@
+package serg;
+
+public class sergeg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("git ");
+		System.out.println("hub");
+	}
+
+}
